0ed36bab45cd88d18019ad51f9fb372f66e85f90
nikomax <maks.chuprina@gmail.com>
2017-09-29 15:04:56 +0300

    stagger

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,24 @@ import 'slick-carousel';
 import {TimelineMax} from 'gsap';
 $(document).ready(function() {
   var tl = new TimelineMax();
-  tl.to('.header__logo, .nav__item',2,{rotation: 360,scale: 0.1})
-    .to('.header__logo, .nav__item',3,{scale:1,onComplete: function() {
-    }});
+  tl.staggerFrom('.header__logo, .nav__item, .aside__image', 2, {scale:0.5, opacity:0, delay:0.1, ease:Elastic.easeOut, force3D:true}, 0.2);
+
+  $('.responsive').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
 });
 
-$('.responsive').slick({
-  arrows: true,
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false
-      }
-    }
-  ]
-});
+
