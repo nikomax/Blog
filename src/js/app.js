15bb73aad5ba6fb8320f1f39aad43def3d8405cc
nikomax <maks.chuprina@gmail.com>
2017-10-01 23:12:39 +0300

    animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
 import $ from 'jquery';
 import 'slick-carousel';
-import {TimelineMax} from 'gsap';
+import { TimelineMax, Elastic, TweenLite } from 'gsap';
+
 $(document).ready(function() {
   var tl = new TimelineMax();
-  tl.staggerFrom('.header__logo, .nav__item, .aside__image', 2, {scale:0.5, opacity:0, delay:0.1, ease:Elastic.easeOut, force3D:true}, 0.2);
+  tl.staggerFrom('.header__logo, .nav__item, .bg, .page-heading, .aside, .aside__image, .section, .image, .content, .pagination__item', 2, {scale:0.5, opacity:0, delay:0.1, ease:Elastic.easeOut, force3D:true}, 0.1);
+
+  $('.header__logo').hover(
+    function() {
+      TweenLite.to($(this), 1.2, {rotationY:360, ease:Back.easeOut});
+    },
+    function() {
+      TweenLite.to($(this), 1.2, {rotationY:0, ease:Back.easeOut});
+    }
+  );
 
   $('.responsive').slick({
     arrows: true,
